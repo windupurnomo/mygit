e3357bfd048f68fca45f4187a3da6863cb63e8c0
Windu Purnomo <wpwindupurnomo@gmail.com>
2016-11-30 10:11:43 +0700

    ui router

---
+++ js/config.router.js
@@ -0,0 +1,17 @@
+'use strict';
+angular.module('app', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'tpl/home.html'
+    })
+
+    .state('about', {
+        url: '/about',
+        templateUrl: 'tpl/about.html'
+    })
+
+});
