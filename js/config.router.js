f34273a12ec632de2a315e2ca693e1e4c30d7055
Amin <ora.genah.ti@gmail.com>
2017-01-23 02:06:14 +0700

    --all

--- js/config.router.js
+++ js/config.router.js
@@ -27,4 +27,54 @@ angular.module('app', ['ui.router'])
         templateUrl: 'tpl/zcode.html'
     })
 
+    .state('profile', {
+        url: '/profile',
+        templateUrl: 'tpl/profile.html'
+    })
+
+    .state('users', {
+        url: '/users',
+        templateUrl: 'tpl/users.html'
+    })
+
+    .state('user', {
+        url: '/user',
+        templateUrl: 'tpl/user.html'
+    })
+
+    .state('question', {
+        url: '/question',
+        templateUrl: 'tpl/question.html'
+    })
+
+    .state('questions', {
+        url: '/questions',
+        templateUrl: 'tpl/questions.html'
+    })
+
+    .state('question-detail', {
+        url: '/question-detail',
+        templateUrl: 'tpl/question-detail.html'
+    })
+
+    .state('exams', {
+        url: '/exams',
+        templateUrl: 'tpl/exams.html'
+    })
+
+    .state('myexams', {
+        url: '/myexams',
+        templateUrl: 'tpl/myexams.html'
+    })
+
+    .state('exam-detail', {
+        url: '/exam-detail',
+        templateUrl: 'tpl/exam-detail.html'
+    })
+
+    .state('exam', {
+        url: '/exam',
+        templateUrl: 'tpl/exam.html'
+    })
+
 });
